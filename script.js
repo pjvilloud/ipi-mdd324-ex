f68f65a090331e629a8492f74f892e382e082e06
pijey <pjvilloud-bitbucket@yahoo.fr>
2018-12-04 19:22:27 +0100

    test

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ $( document ).ready(function() {
     		url:'https://3zw9c5mdo8.execute-api.eu-west-2.amazonaws.com/default/getbitcoinrate', 
     		data: {bitcoinAmount: 5,currencyList:"USD,EUR"}, 
     		headers: {
-    			'Access-Control-Allow-Origin': "*"
+                'Accept': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest'
     		},
     		success: function(data, textStatus, xhr) {
 		    	/*optional stuff to do after success */
