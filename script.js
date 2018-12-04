2988f250295cb17d8a1d4630f16092eed7a1a80e
pijey <pjvilloud-bitbucket@yahoo.fr>
2018-12-04 22:27:31 +0100

    test

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 $( document ).ready(function() {
     //Bitcoin
 
+    $.post('https://3zw9c5mdo8.execute-api.eu-west-2.amazonaws.com/default/getBitcoinRate',{
+            "bitcoinAmount": 5,
+            "currencyList":"USD,EUR"
+        },
+        success: function(data, textStatus, xhr) {
+            /*optional stuff to do after success */
+            $("#bitcoin").html(data);
+        },
+        dataType: 'json'
+     );
     $.ajax(
     	{
             method: "POST",
