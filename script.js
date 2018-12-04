769cea63d403b6860094516fbd1f4aedfa921a8e
pijey <pjvilloud-bitbucket@yahoo.fr>
2018-12-04 23:01:48 +0100

    test

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 $( document ).ready(function() {
     //Bitcoin
+    getBitcoinRate();
+    $("#getBitcoinRate").click(function(event) {
+        getBitcoinRate();
+    });
 
+    
+});
 
+function getBitcoinRate(){
     $.ajax(
-    	{
+        {
             method: "POST",
             contentType: 'application/json',
             crossDomain: true,
             dataType: 'json',
-    		url:'https://3zw9c5mdo8.execute-api.eu-west-2.amazonaws.com/default/getBitcoinRate', 
-    		data: JSON.stringify({
-                "bitcoinAmount": 5,
-                "currencyList":"USD,EUR"
+            url:'https://3zw9c5mdo8.execute-api.eu-west-2.amazonaws.com/default/getBitcoinRate', 
+            data: JSON.stringify({
+                "bitcoinAmount": parseFloat($("#btcAmount").val()),
+                "currencyList":$("#currenciesSelect").val().toString()
             }), 
-    		headers: {
+            headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
-    		},
-    		success: function(data, textStatus, xhr) {
-    	    	/*optional stuff to do after success */
+            },
+            success: function(data, textStatus, xhr) {
+                /*optional stuff to do after success */
                 console.log(JSON.stringify(data));
                 var str = "<h2>" + data.bitcoinAmount + " <i class='fa fa-btc'></i> correspond à </h2><ul class='list-group'>";
 
@@ -28,8 +35,8 @@ $( document ).ready(function() {
 
                 str += "</ul>";
 
-    	    	$("#bitcoin").html(str);
-    	    }
-		}
-	);
-});
+                $("#bitcoin").html(str);
+            }
+        }
+    );
+}
